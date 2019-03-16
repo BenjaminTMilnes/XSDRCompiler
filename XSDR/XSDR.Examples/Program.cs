8c9b7694fcf52714bea29f7d0d02c2e3c68f5d68
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2019-03-16 14:04:07 +0000

    Got basic example working

--- XSDR/XSDR.Examples/Program.cs
+++ XSDR/XSDR.Examples/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using XSDR;
 
 namespace XSDR.Examples
 {
@@ -10,6 +12,11 @@ namespace XSDR.Examples
     {
         static void Main(string[] args)
         {
+            var wordExporter = new WordExporter();
+
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "../../Examples/example0.docx");
+
+            wordExporter.ExportXSDRDocument(new XSDRDocument(), filePath);
         }
     }
 }
