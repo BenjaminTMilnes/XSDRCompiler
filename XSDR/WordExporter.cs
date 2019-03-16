2a5cf0d5f110347c3bd8c24ee5eefcb156545984
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2019-03-16 13:59:04 +0000

    Began adding importers and exporters

---
+++ XSDR/WordExporter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace XSDR
+{
+    public class WordExporter
+    {
+        public void ExportXSDRDocument(XSDRDocument document, string filePath)
+        {
+            using (WordprocessingDocument wordDocument = WordprocessingDocument.Create(filePath, WordprocessingDocumentType.Document))
+            {
+                var mainPart = wordDocument.AddMainDocumentPart();
+
+                mainPart.Document = new Document();
+
+                var body = mainPart.Document.AppendChild(new Body());
+                var paragraph = body.AppendChild(new Paragraph());
+                var run = paragraph.AppendChild(new Run());
+                run.AppendChild(new Text("Hello world"));
+            }
+        }
+    }
+}
