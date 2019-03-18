4f0a1d8219d44f03ebdc0364157e21f4cff96043
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2019-03-18 20:37:28 +0000

    Began adding page size and margin controls

--- XSDR/WordExporter.cs
+++ XSDR/WordExporter.cs
@@ -19,8 +19,17 @@ namespace XSDR
             return new FontSize() { Val = (fontHeight * 2).ToString() };
         }
 
+             private  int ConvertFromMillimetres(double mm)
+        {
+            return (int)Math.Round(mm * 72 * 20 / 25.4);
+        }
+        
         public void ExportXSDRDocument(XSDRDocument document, string filePath)
         {
+            OpenSettings openSettings = new OpenSettings();
+
+            openSettings.MarkupCompatibilityProcessSettings = new MarkupCompatibilityProcessSettings(MarkupCompatibilityProcessMode.ProcessAllParts, FileFormatVersions.Office2013);
+
             using (WordprocessingDocument wordDocument = WordprocessingDocument.Create(filePath, WordprocessingDocumentType.Document))
             {
                 var mainPart = wordDocument.AddMainDocumentPart();
@@ -29,11 +38,22 @@ namespace XSDR
 
                 var body = mainPart.Document.AppendChild(new Body());
 
+                var sectionProperties = new SectionProperties();
+                var pageSize = new PageSize();
+                pageSize.Width = (uint) ConvertFromMillimetres(128.5);
+                pageSize.Height = (uint)ConvertFromMillimetres(198.4);
+
+                var pageMargin = new PageMargin() { Top =  ConvertFromMillimetres(20), Bottom = ConvertFromMillimetres(20), Left = (uint) ConvertFromMillimetres(20), Right = (uint) ConvertFromMillimetres(20) };
+
+                sectionProperties.Append(pageSize);
+                sectionProperties.Append(pageMargin);
+                body.Append(sectionProperties);
+
                 foreach (var s in document.Sections)
                 {
                     foreach (var e1 in s.Subelements)
                     {
-                        if (e1 is XSDRParagraph || e1 is XSDRHeading )
+                        if (e1 is XSDRParagraph || e1 is XSDRHeading)
                         {
                             var paragraph = body.AppendChild(new Paragraph());
 
@@ -43,65 +63,76 @@ namespace XSDR
                                 {
                                     var text = (e2 as XSDRTextElement).Text;
 
-                                    var run = paragraph.AppendChild(new Run());
-                                    var runProperties = new RunProperties(new RunFonts() { Ascii = _defaultFontName }, _getFontSize(_defaultFontHeight));
-
-                                    run.PrependChild(runProperties);
-                                    run.AppendChild(new Text() { Text = text, Space = SpaceProcessingModeValues.Preserve });
+                                    AddRunToParagraph(paragraph, text, _defaultFontName, _defaultFontHeight);
                                 }
                                 if (e2 is XSDRItalic)
                                 {
                                     var text = ((e2 as XSDRItalic).Subelements[0] as XSDRTextElement).Text;
 
-                                    var run = paragraph.AppendChild(new Run());
-                                    var runProperties = new RunProperties(new RunFonts() { Ascii = _defaultFontName }, _getFontSize(_defaultFontHeight), new Italic());
-
-                                    run.PrependChild(runProperties);
-                                    run.AppendChild(new Text() { Text = text, Space = SpaceProcessingModeValues.Preserve });
+                                    AddRunToParagraph(paragraph, text, _defaultFontName, _defaultFontHeight, true);
                                 }
                                 if (e2 is XSDRBold)
                                 {
                                     var text = ((e2 as XSDRBold).Subelements[0] as XSDRTextElement).Text;
 
-                                    var run = paragraph.AppendChild(new Run());
-                                    var runProperties = new RunProperties(new RunFonts() { Ascii = _defaultFontName }, _getFontSize(_defaultFontHeight), new Bold());
-
-                                    run.PrependChild(runProperties);
-                                    run.AppendChild(new Text() { Text = text, Space = SpaceProcessingModeValues.Preserve });
+                                    AddRunToParagraph(paragraph, text, _defaultFontName, _defaultFontHeight, false, true);
                                 }
                                 if (e2 is XSDRUnderline)
                                 {
                                     var text = ((e2 as XSDRUnderline).Subelements[0] as XSDRTextElement).Text;
 
-                                    var run = paragraph.AppendChild(new Run());
-                                    var runProperties = new RunProperties(new RunFonts() { Ascii = _defaultFontName }, _getFontSize(_defaultFontHeight), new Underline());
-
-                                    run.PrependChild(runProperties);
-                                    run.AppendChild(new Text() { Text = text, Space = SpaceProcessingModeValues.Preserve });
+                                    AddRunToParagraph(paragraph, text, _defaultFontName, _defaultFontHeight, false, false, true);
                                 }
                                 if (e2 is XSDRStrikethrough)
                                 {
                                     var text = ((e2 as XSDRStrikethrough).Subelements[0] as XSDRTextElement).Text;
 
-                                    var run = paragraph.AppendChild(new Run());
-                                    var runProperties = new RunProperties(new RunFonts() { Ascii = _defaultFontName }, _getFontSize(_defaultFontHeight), new Strike());
-
-                                    run.PrependChild(runProperties);
-                                    run.AppendChild(new Text() { Text = text, Space = SpaceProcessingModeValues.Preserve });
+                                    AddRunToParagraph(paragraph, text, _defaultFontName, _defaultFontHeight, false, false, false, true);
                                 }
-                              
+
                             }
                         }
                         if (e1 is XSDRPageBreak)
                         {
-                            var paragraph = body.AppendChild(new Paragraph());
-                            var run = paragraph.AppendChild(new Run());
-
-                            run.AppendChild(new Break() { Type = BreakValues.Page });
+                            AddPageBreakToBody(body);
                         }
                     }
                 }
             }
+        }
+
+        public void AddPageBreakToBody(Body body)
+        {
+            var paragraph = body.AppendChild(new Paragraph());
+            var run = paragraph.AppendChild(new Run());
+
+            run.AppendChild(new Break() { Type = BreakValues.Page });
+        }
+
+        public void AddRunToParagraph(Paragraph paragraph, string text, string fontName, int fontHeight, bool isItalic = false, bool isBold = false, bool isUnderlined = false, bool isStruckThrough = false)
+        {
+            var run = paragraph.AppendChild(new Run());
+            var runProperties = new RunProperties(new RunFonts() { Ascii = fontName }, _getFontSize(fontHeight));
+
+            if (isItalic)
+            {
+                runProperties.Append(new Italic());
+            }
+            if (isBold)
+            {
+                runProperties.Append(new Bold());
+            }
+            if (isUnderlined)
+            {
+                runProperties.Append(new Underline());
+            }
+            if (isStruckThrough)
+            {
+                runProperties.Append(new Strike());
+            }
+
+            run.PrependChild(runProperties);
+            run.AppendChild(new Text() { Text = text, Space = SpaceProcessingModeValues.Preserve });
         }
     }
 }
